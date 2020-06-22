518f706c1198fb3a653ef2608bd529b8b680bcff
Sung <gch03944@gmail.com>
2020-06-22 22:17:27 +0900

    add checkStyle3

--- src/main/java/com/sgh/calculator/CalculatorApplication.java
+++ src/main/java/com/sgh/calculator/CalculatorApplication.java
@@ -3,6 +3,10 @@ package com.sgh.calculator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * This is bb
+ */
+
 @SpringBootApplication
 public class CalculatorApplication {
 
