0f9fdc9b66fd9cdfba497018b1c14123ef109c4e
Sung <gch03944@gmail.com>
2020-06-22 22:13:31 +0900

    add checkStyle2

--- src/main/java/com/sgh/calculator/Calculator.java
+++ src/main/java/com/sgh/calculator/Calculator.java
@@ -1,6 +1,11 @@
 package com.sgh.calculator;
 import org.springframework.stereotype.Service;
+/**
+ * Thisis a JavaDoc Comment for Calculator class
+ */
+
 @Service
+
 public class Calculator {
 int sum(int a, int b) {
 return a + b;
